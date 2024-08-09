4c07aac958060af7863444ddd5e23e7601d0ce32
EmmAdeliom <emma.louviot@adeliom.com>
2024-08-09 11:33:35 +0200

    feat(block): enhanced formblock

--- web/app/themes/adeliom/app/Blocks/Action/FormBlock.php
+++ web/app/themes/adeliom/app/Blocks/Action/FormBlock.php
@@ -6,11 +6,17 @@ namespace App\Blocks\Action;
 
 use Adeliom\HorizonTools\Blocks\AbstractBlock;
 use Adeliom\HorizonTools\Fields\Buttons\ButtonField;
+use Adeliom\HorizonTools\Fields\Layout\LayoutField;
 use Adeliom\HorizonTools\Fields\Tabs\ContentTab;
+use Adeliom\HorizonTools\Fields\Tabs\LayoutTab;
 use Adeliom\HorizonTools\Fields\Text\HeadingField;
 use Adeliom\HorizonTools\Services\FormService;
 use App\Fields\FormField;
 use App\Fields\OfferField;
+use Extended\ACF\ConditionalLogic;
+use Extended\ACF\Fields\Image;
+use Extended\ACF\Fields\RadioButton;
+use Extended\ACF\Fields\Text;
 
 class FormBlock extends AbstractBlock
 {
@@ -18,12 +24,46 @@ class FormBlock extends AbstractBlock
     public static ?string $title = 'Formulaire';
     public static ?string $mode = 'preview';
 
+    final public const string FIELD_DESC = "desc";
+    final public const string FIELD_POSITION = "position";
+    final public const string POSITION_LEFT = "left";
+    final public const string POSITION_CENTER = "center";
+
+
+    final public const string FIELD_BG_TYPE = 'bg-type';
+    final public const string FIELD_BG_IMAGE = 'bg-image';
+    final public const string BG_COLOR_TYPE = "bg-color-type";
+    final public const string BG_IMAGE_TYPE = "bg-image-type";
+
     public function getFields(): ?iterable
     {
         yield from ContentTab::make()->fields([
             HeadingField::make()->required(),
+            Text::make('Description', self::FIELD_DESC),
             OfferField::make(),
-            FormField::selectGF(FormService::getAllFormChoices())
+            FormField::selectGF(FormService::getAllFormChoices()),
+        ]);
+
+        yield from LayoutTab::make()->fields([
+            LayoutField::margin(),
+            RadioButton::make('Position', self::FIELD_POSITION,)
+                ->choices([
+                    self::POSITION_LEFT   => 'Gauche',
+                    self::POSITION_CENTER => 'Centre',
+                ])
+                ->default(self::POSITION_LEFT),
+
+            RadioButton::make("Type de fond", self::FIELD_BG_TYPE)
+                ->choices([
+                    self::BG_COLOR_TYPE => 'Couleur',
+                    self::BG_IMAGE_TYPE => 'Image',
+                ]),
+
+            Image::make("Image de fond", self::FIELD_BG_IMAGE)
+                ->conditionalLogic([
+                    ConditionalLogic::where(self::FIELD_BG_TYPE, '==', self::BG_IMAGE_TYPE),
+                ]),
+
         ]);
     }
 }
--- web/app/themes/adeliom/resources/views/blocks/action/form.blade.php
+++ web/app/themes/adeliom/resources/views/blocks/action/form.blade.php
@@ -1,20 +1,33 @@
-<x-block :fields="$fields">
-    <div class="container">
-        <div class="grid gap-6 lg:grid-cols-12">
-            <div class="lg:col-span-5">
-                <x-heading :fields="$fields['title']"/>
-            </div>
+@php
+ $positionClass = $fields['position'] != 'left' ? 'lg:col-start-3' : '';
+  $bgType = $fields['bg-type'] ?? 'bg-color-type';
+  $bgColor = $bgType  === "bg-color-type" ? "bg-color-02-50" : "";
+  $bgImage = ($bgType === "bg-image-type" && isset($fields['bg-image'])) ? $fields['bg-image']['sizes']['large'] : "";
+@endphp
 
-            <div class="lg:col-span-7">
-                @if($fields['offer'] && $fields['offer']['enable'])
-                    <x-offer :fields="$fields['offer']"/>
-                @endif
+<x-block :fields="$fields" class="{{$bgColor}} relative" background="none">
+    @if($bgImage)
+        <div class="absolute inset-0 bg-cover bg-center z-0" style="background-image: url('{{ $bgImage }}')"></div>
+    @endif
+    <div class="container z-10 relative">
+        <div class="grid-12">
+            <div class="lg:col-span-8 {{$positionClass}}">
+                <div class="bg-white rounded-card p-10">
+                    <x-typography.heading :fields="$fields['title']" size="5"/>
 
-                @if($fields['form_id'])
-                    @php
-                        echo do_shortcode('[gravityform id="'.$fields['form_id'].'" title="false" description="false" ajax="true"]');
-                    @endphp
-                @endif
+                    @if($fields['desc'])
+                        <x-typography.text :content="$fields['desc']" />
+                    @endif
+                    @if($fields['offer'] && $fields['offer']['enable'])
+                        <x-offer :fields="$fields['offer']"/>
+                    @endif
+
+                    @if($fields['form_id'])
+                        @php
+                            echo do_shortcode('[gravityform id="'.$fields['form_id'].'" title="false" description="false" ajax="true"]');
+                        @endphp
+                    @endif
+                </div>
             </div>
         </div>
     </div>
