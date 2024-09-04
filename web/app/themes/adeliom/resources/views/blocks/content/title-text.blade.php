639194328885e4c91f92633c56532a4bcb6cce7b
emmadeliom <emma.louviot@adeliom.com>
2024-09-04 16:25:58 +0200

    feat(block): TitleTextBlock.php - BO

--- web/app/themes/adeliom/resources/views/blocks/content/title-text.blade.php
+++ web/app/themes/adeliom/resources/views/blocks/content/title-text.blade.php
@@ -1,10 +1,23 @@
 <x-block :fields="$fields">
-    <div class="grid-12">
-        <div class="lg:col-span-5">
-            <x-typography.heading :fields="$fields['title']" />
-        </div>
-        <div class="lg:col-span-7">
-            <x-typography.text :content="$fields['wysiwyg']" />
-        </div>
-    </div>
+	<div class="grid-12">
+		<div class="lg:col-span-5">
+			@isset($fields['uptitle'])
+				<x-typography.uptitle :content="$fields['uptitle']"/>
+			@endisset
+			
+			@isset($fields['title'])
+				<x-typography.heading :fields="$fields['title']"/>
+			@endisset
+		</div>
+		<div class="lg:col-span-7">
+			@isset($fields['wysiwyg'])
+				<x-typography.text :content="$fields['wysiwyg']" class=""/>
+			@endisset
+			
+			@isset($fields['buttons'])
+				<x-action.buttons :buttons="$fields['buttons']"/>
+			@endisset
+		
+		</div>
+	</div>
 </x-block>
