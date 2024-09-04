a517489a05d8f2f0b36051ab7a12972caf44ad65
emmadeliom <emma.louviot@adeliom.com>
2024-09-04 16:13:20 +0200

    feat(block): CardsBlock.php - BO

--- web/app/themes/adeliom/resources/views/components/cards/card-basic.blade.php
+++ web/app/themes/adeliom/resources/views/components/cards/card-basic.blade.php
@@ -1,12 +1,15 @@
 <div class="col-span-6 bg-full pt-24 relative rounded-card"
-    style="background-image: url({{ $card['img']['sizes']['large'] }})">
-    <div class="absolute-full bg-linear rounded-card"></div>
-    <div class="p-card flex flex-col items-start gap-card shadow-small-blur relative z-10">
-        <x-heading :fields="$card['title']" :size="5" class="awc-theme-dark" />
-        {{-- Passer en composant --}}
-        <div class="wysiwyg">
-            {!! $card['wysiwyg'] !!}
-        </div>
-        <x-action.button :fields="$card['button']" />
-    </div>
+     style="background-image: url({{ $card['img']['sizes']['large'] ?? '' }})">
+	<div class="absolute-full bg-linear rounded-card"></div>
+	<div class="p-card flex flex-col items-start gap-card shadow-small-blur relative z-10">
+		@isset($card['title'])
+			<x-typography.heading :fields="$card['title']" :size="5" class="awc-theme-dark"/>
+		@endisset
+		@isset($card['wysiwyg'])
+			<x-typography.text :content="$card['wysiwyg']" class=""/>
+		@endisset
+		@isset($card['button'])
+			<x-action.button :object="$card['button']"/>
+		@endisset
+	</div>
 </div>
