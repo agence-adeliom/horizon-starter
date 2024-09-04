0641cf101c153dba650b6d2d368d98e24eede3d2
emmadeliom <emma.louviot@adeliom.com>
2024-09-04 16:25:58 +0200

    feat(block): TitleTextBlock.php - BO

--- web/app/themes/adeliom/app/Blocks/Content/TitleTextBlock.php
+++ web/app/themes/adeliom/app/Blocks/Content/TitleTextBlock.php
@@ -5,29 +5,31 @@ declare(strict_types=1);
 namespace App\Blocks\Content;
 
 use Adeliom\HorizonTools\Blocks\AbstractBlock;
+use Adeliom\HorizonTools\Fields\Buttons\ButtonField;
 use Adeliom\HorizonTools\Fields\Layout\LayoutField;
 use Adeliom\HorizonTools\Fields\Tabs\ContentTab;
 use Adeliom\HorizonTools\Fields\Tabs\LayoutTab;
 use Adeliom\HorizonTools\Fields\Text\HeadingField;
+use Adeliom\HorizonTools\Fields\Text\UptitleField;
 use Adeliom\HorizonTools\Fields\Text\WysiwygField;
 
 class TitleTextBlock extends AbstractBlock
 {
     public static ?string $slug = 'title-text';
     public static ?string $title = 'Titre texte';
-    public static ?string $mode = 'preview';
     public static ?string $icon = 'analytics';
 
     public function getFields(): ?iterable
     {
         yield from ContentTab::make()->fields([
+            UptitleField::make(),
             HeadingField::make()->required(),
-            WysiwygField::make()
+            WysiwygField::default(),
+            ButtonField::group(),
         ]);
 
         yield from LayoutTab::make()->fields([
             LayoutField::margin(),
-            LayoutField::darkMode(),
         ]);
     }
 }
