7a3c92163ca206521f99268e7d487d9ff9109fc4
Davy JACQUEMARD <davy.jacquemard@adeliom.com>
2024-09-27 17:01:55 +0200

    feat(block): TitleTextBlock - FO

--- web/app/themes/adeliom/resources/views/blocks/content/title-text.blade.php
+++ web/app/themes/adeliom/resources/views/blocks/content/title-text.blade.php
@@ -1,23 +1,23 @@
 <x-block :fields="$fields">
-	<div class="grid-12">
-		<div class="lg:col-span-5">
-			@isset($fields['uptitle'])
-				<x-typography.uptitle :content="$fields['uptitle']"/>
-			@endisset
-			
-			@isset($fields['title'])
-				<x-typography.heading :fields="$fields['title']"/>
-			@endisset
-		</div>
-		<div class="lg:col-span-7">
-			@isset($fields['wysiwyg'])
-				<x-typography.text :content="$fields['wysiwyg']" class=""/>
-			@endisset
-			
-			@isset($fields['buttons'])
-				<x-action.buttons :buttons="$fields['buttons']"/>
-			@endisset
-		
-		</div>
-	</div>
+    <div class="grid-12">
+        <div class="lg:col-span-5">
+            @isset($fields['uptitle'])
+                <x-typography.uptitle :content="$fields['uptitle']" />
+            @endisset
+
+            @isset($fields['title'])
+                <x-typography.heading :fields="$fields['title']" size="3" />
+            @endisset
+        </div>
+        <div class="lg:col-span-7">
+            @isset($fields['wysiwyg'])
+                <x-typography.text :content="$fields['wysiwyg']" />
+            @endisset
+
+            @isset($fields['buttons'])
+                <x-action.buttons :buttons="$fields['buttons']" class="mt-button-text-mobile lg:mt-button-text-desktop" />
+            @endisset
+
+        </div>
+    </div>
 </x-block>
--- web/app/themes/adeliom/resources/views/components/action/buttons.blade.php
+++ web/app/themes/adeliom/resources/views/components/action/buttons.blade.php
@@ -1,11 +1,11 @@
-@if($buttons)
-  <div class="flex flex-col md:flex-row gap-4{{ $attributes['class'] ? ' '.$attributes['class'] : null }}">
-    @foreach($buttons as $button)
-      @if($loop->first)
-        <x-action.button :object="$button" type="primary"/>
-      @else
-        <x-action.button :object="$button" type="secondary"/>
-      @endif
-    @endforeach
-  </div>
+@if ($buttons)
+    <div class="flex flex-wrap gap-4 {{ $attributes['class'] ? ' ' . $attributes['class'] : null }}">
+        @foreach ($buttons as $button)
+            @if ($loop->first)
+                <x-action.button :object="$button" type="primary" class="max-lg:flex-1" />
+            @else
+                <x-action.button :object="$button" type="secondary" class="max-lg:flex-1" />
+            @endif
+        @endforeach
+    </div>
 @endif
