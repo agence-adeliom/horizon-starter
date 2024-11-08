dc6b433c22e3b56e3df302e5e89636628b3a90ca
Davy JACQUEMARD <davy.jacquemard@adeliom.com>
2024-11-08 17:13:59 +0100

    fix(banner): remove link from lp

--- web/app/themes/adeliom/resources/scripts/editor.ts
+++ web/app/themes/adeliom/resources/scripts/editor.ts
@@ -1,9 +1,11 @@
 /**
  * @see {@link https://bud.js.org/extensions/bud-preset-wordpress/editor-integration/filters}
  */
+//@ts-expect-error
 roots.register.filters('@scripts/filters');
 
 /**
  * @see {@link https://webpack.js.org/api/hot-module-replacement/}
  */
+//@ts-expect-error
 if (import.meta.webpackHot) import.meta.webpackHot.accept(console.error);
