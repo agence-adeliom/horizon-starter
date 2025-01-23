0d71b2ab80828abfd8e919f9102bd9661fd7dd57
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-01-23 12:35:47 +0100

    feat(horizon-tools): update to latest version for automatic script import
    Updated the Horizon Tools version to manage automatic import of Livewire
    scripts and styles. Adjusted layout and composer files for compatibility
    and improved structure in the application's blade templates.

--- web/app/themes/adeliom/resources/views/layouts/app.blade.php
+++ web/app/themes/adeliom/resources/views/layouts/app.blade.php
@@ -6,16 +6,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	
-	{{--     Following link to change depending on project  --}}
+
+  {{--     Following link to change depending on project  --}}
 	<link
 			href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
 			rel="stylesheet">
 	{{--    End   --}}
-	
-	@php(do_action('get_header'))
+
+  @php(do_action('get_header'))
 	@php(wp_head())
-	@livewireStyles
 </head>
 
 <body @php(body_class()) x-data="initPage()">
@@ -25,20 +24,20 @@
 	<a class="sr-only focus:not-sr-only" href="#main">
 		{{ __('Skip to content') }}
 	</a>
-	
-	@include('sections.promo-banner')
-	
-	@if ($isLp)
+
+  @include('sections.promo-banner')
+
+@if ($isLp)
 		@include('sections.header-lp')
 	@else
 		@include('sections.header')
 	@endif
-	
-	<main id="main" class="main">
+
+  <main id="main" class="main">
 		@yield('content')
 	</main>
-	
-	@if ($isLp)
+
+@if ($isLp)
 		@include('sections.footer-lp')
 	@else
 		@include('sections.footer')
@@ -47,7 +46,6 @@
 
 @php(do_action('get_footer'))
 @php(wp_footer())
-@livewireScripts
 </body>
 
 </html>
