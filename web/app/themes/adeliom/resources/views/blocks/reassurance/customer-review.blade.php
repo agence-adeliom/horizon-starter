7057cb2c9c14d2dbff88059b98ef7032ee928ccf
Davy JACQUEMARD <davy.jacquemard@adeliom.com>
2025-01-24 11:40:12 +0100

    fix(customers-reviews): remove useless variables

--- web/app/themes/adeliom/resources/views/blocks/reassurance/customer-review.blade.php
+++ web/app/themes/adeliom/resources/views/blocks/reassurance/customer-review.blade.php
@@ -1,12 +1,11 @@
 @if ($fields)
     <x-block :fields="$fields" class="overflow-hidden">
 
-        <div class="flex w-full flex-col gap-text-image-mobile lg:gap-text-image-desktop">
+        <div class="flex w-full flex-col gap-8 lg:gap-10">
             <div class="grid-12">
                 <div class="col-span-full flex flex-col items-center text-center lg:col-span-8 lg:col-start-3">
                     @isset($fields['uptitle'])
-                        <x-typography.uptitle :content="$fields['uptitle']"
-                            class="mb-headline-title-mobile lg:mb-headline-title-desktop" />
+                        <x-typography.uptitle :content="$fields['uptitle']" class="mb-1" />
                     @endisset
 
                     @isset($fields['title'])
@@ -14,14 +13,12 @@
                     @endisset
 
                     @isset($fields['wysiwyg'])
-                        <x-typography.text :content="$fields['wysiwyg']"
-                            class="mt-title-text-mobile text-large lg:mt-title-text-desktop" />
+                        <x-typography.text :content="$fields['wysiwyg']" class="mt-2 lg:mt-4" />
                     @endisset
 
                     @if (@isset($context['global-rating']) || @isset($context['btn-reviews']))
 
-                        <div
-                            class="mt-title-text-mobile flex flex-col items-center gap-medium lg:mt-title-text-desktop lg:flex-row lg:gap-xlarge">
+                        <div class="mt-2 flex flex-col items-center gap-4 lg:mt-4 lg:flex-row lg:gap-6">
                             @if (@isset($context['global-rating']) && $context['global-rating'])
                                 <x-ui.rating :score="$context['global-rating']" showScore />
                             @endisset
