e86fbf274687ffed9a1f1837211e25a9b5826009
Davy JACQUEMARD <davy.jacquemard@adeliom.com>
2025-04-14 12:08:37 +0200

    fix(roles): handle editor roles

---
+++ web/app/themes/adeliom/app/Hooks/UserRoleHook.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Hooks;
+
+use Adeliom\HorizonTools\Hooks\AbstractHook;
+
+class UserRoleHook extends AbstractHook
+{
+    public static function addEditorRoles()
+    {
+        $role = get_role('editor');
+        $caps = [
+            //theme options
+            'edit_theme_options',
+            // gravity forms
+            'gravityforms_view_entries',
+            'gravityforms_edit_entries',
+            'gravityforms_delete_entries',
+            'gravityforms_export_entries',
+            'gravityforms_view_entry_notes',
+            'gravityforms_edit_forms',
+            'gravityforms_create_form',
+            'gravityforms_delete_forms',
+            'gravityforms_preview_forms',
+            'gravityforms_edit_settings',
+            'gravityforms_edit_other_forms',
+            'gravityforms_delete_other_forms',
+            'gravityforms_view_forms',
+        ];
+
+        foreach ($caps as $cap) {
+            if (!$role->has_cap($cap)) {
+                $role->add_cap($cap);
+            }
+        }
+    }
+
+    public function init(): void
+    {
+        add_action('admin_init', [
+            $this,
+            'addEditorRoles',
+        ], 10);
+    }
+}
