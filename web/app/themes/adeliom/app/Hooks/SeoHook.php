ae61f15f2db22fa0492dc8a0690ca15fa55e285c
emmadeliom <emma.louviot@adeliom.com>
2025-06-18 13:30:32 +0200

    feat(hook): add SEO breadcrumb customization
    - Implemented a new `SeoHook` class for modifying breadcrumb
      arguments and settings in the Rank Math plugin.
    - Added methods for customizing the HTML output and separator.

---
+++ web/app/themes/adeliom/app/Hooks/SeoHook.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Hooks;
+
+use Adeliom\HorizonTools\Hooks\AbstractHook;
+use BladeUI\Icons\Factory;
+
+class SeoHook extends AbstractHook
+{
+
+
+    public static function rmBreadcrumbArgs($args): array
+    {
+        return array_merge($args, [
+            'wrap_before' => '<nav aria-label="breadcrumbs" id="breadcrumbs" class="main-breadcrumbs">',
+            'wrap_after'  => '</nav>',
+        ]);
+    }
+
+    public static function rmBreadcrumbSettings($args): array
+    {
+        return array_merge($args, [
+            'separator' => static::breadcrumbSeparator(),
+        ]);
+    }
+
+    public static function breadcrumbSeparator(): string
+    {
+        return '';
+    }
+
+    public static function rmBreadcrumbHtml($html, $crumbs, $class): string
+    {
+        $iconFactory = app(Factory::class);
+        $html = preg_replace('/<span class="separator">.*?<\/span>/', $iconFactory->svg('fas:sharp-angle-right', 'separator w-[16px] h-[16px] text-secondary')->toHtml(), $html);
+
+        return $html;
+    }
+
+
+    public function init(): void
+    {
+        add_filter('rank_math/frontend/breadcrumb/args', [
+            $this,
+            'rmBreadcrumbArgs',
+        ], 10, 1);
+
+        add_filter('rank_math/frontend/breadcrumb/settings', [
+            $this,
+            'rmBreadcrumbSettings',
+        ], 10, 1);
+        add_filter('rank_math/frontend/breadcrumb/html', [
+            $this,
+            'rmBreadcrumbHtml',
+        ], 10, 3);
+    }
+}
