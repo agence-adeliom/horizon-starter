d247125ab73b648e68480502c96c1bdd972bd634
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-07-09 15:15:40 +0200

    feat(back-office): add initial configuration for back-office settings

---
+++ web/app/themes/adeliom/config/back-office.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'login' => [
+        'header' => [
+            'url' => null,
+            'title' => null,
+            'logo' => [
+                'url' => null,
+                'height' => 120,
+                'width' => 120,
+                'radius' => 120,
+                'backgroundColor' => '#FFFFFF',
+            ]
+        ]
+    ],
+    'bo' => [
+        'footerText' => null,
+    ],
+];
