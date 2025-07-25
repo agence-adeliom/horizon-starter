eccc8d0f4762ea0cdf722f35010997160563dbdf
emmadeliom <emma.louviot@adeliom.com>
2025-07-25 12:53:06 +0200

    fix(highlightBlock): update layout and structure for highlight block
    - Improved grid layout and column span for better responsiveness.
    - Updated icon to 'lightbulb' for highlight block.
    - Cleaned up unused imports from HighlightBlock class.

--- web/app/themes/adeliom/app/Blocks/Content/HighlightBlock.php
+++ web/app/themes/adeliom/app/Blocks/Content/HighlightBlock.php
@@ -5,21 +5,21 @@ declare(strict_types=1);
 namespace App\Blocks\Content;
 
 use Adeliom\HorizonTools\Blocks\AbstractBlock;
+use Adeliom\HorizonTools\Fields\Buttons\ButtonField;
+use Adeliom\HorizonTools\Fields\Layout\LayoutField;
 use Adeliom\HorizonTools\Fields\Tabs\ContentTab;
+use Adeliom\HorizonTools\Fields\Tabs\LayoutTab;
+use Adeliom\HorizonTools\Fields\Tabs\MediaTab;
 use Adeliom\HorizonTools\Fields\Text\HeadingField;
 use Adeliom\HorizonTools\Fields\Text\UptitleField;
 use Adeliom\HorizonTools\Fields\Text\WysiwygField;
-use Adeliom\HorizonTools\Fields\Buttons\ButtonField;
-use Adeliom\HorizonTools\Fields\Tabs\MediaTab;
 use Extended\ACF\Fields\Image;
-use Adeliom\HorizonTools\Fields\Tabs\LayoutTab;
-use Adeliom\HorizonTools\Fields\Layout\LayoutField;
 
 class HighlightBlock extends AbstractBlock
-{    
+{
     public const string FIELD_MAIN_IMAGE = "main_image";
     public static ?string $slug = 'highlight';
-    public static ?string $icon = 'dashicons-warning';
+    public static ?string $icon = 'lightbulb';
     public static ?string $title = 'Mise en avant';
     public static ?string $mode = 'preview';
     public static ?string $description = 'Valorise un contenu spécifique comme une offre, un produit ou une information, afin d’attirer l’attention et guider vers le bouton CTA.';
@@ -37,7 +37,6 @@ class HighlightBlock extends AbstractBlock
         ]);
 
         yield from LayoutTab::make()->fields([
-            LayoutField::darkMode(),
             LayoutField::margin(),
         ]);
     }
