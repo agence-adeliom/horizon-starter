6921008dcc3d793449a611118b866b405d95c16f
Lucas Vigneron <lucas.vigneron@adeliom.com>
2025-07-29 14:09:01 +0200

    feat(search-engine): update search form to use dynamic URL and parameter from SearchEngineService

--- web/app/themes/adeliom/resources/views/components/form/search.blade.php
+++ web/app/themes/adeliom/resources/views/components/form/search.blade.php
@@ -1,9 +1,14 @@
-<form action="" @class([
+@php use Adeliom\HorizonTools\Services\SearchEngineService; @endphp
+
+<form action="{{SearchEngineService::getSearchEngineResultsUrl()}}" @class([
     'w-full flex flex-col sm:flex-row sm:justify-center gap-3',
     $attributes->get('class'),
 ])>
 
-    <x-form.input :x-init="$isModal ? '$watch(\'open\', value => $focus.focus($el))' : ''" name="search" label="Recherche" hideLabel type="text" wrapper-class="flex-1"
-        class="h-[42px]" placeholder="Tapez un mot clé..." required />
+    <x-form.input :x-init="$isModal ? '$watch(\'open\', value => $focus.focus($el))' : ''"
+                  :name="SearchEngineService::getSearchEngineGETParameter()"
+                  label="Recherche" hideLabel type="text" wrapper-class="flex-1"
+                  class="h-[42px]" placeholder="Tapez un mot clé..." required />
+
     <x-action.button icon="fas-search" type="primary" tag="button" submit>Rechercher</x-action.button>
 </form>
